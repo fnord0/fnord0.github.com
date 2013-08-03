46eb9882953e1fba9873667c465ea32bb990791f
fnord0 <fnord0@riseup.net>
2013-08-02 23:23:16 -0700

    site test 1

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,21 +5,22 @@ from __future__ import unicode_literals
 from os import environ
 
 
-SITENAME = u'Kevin Richardson'
-AUTHOR = u'Kevin Richardson'
-TAGLINE = u'explorer & tinkerer'
+SITENAME = u'fnord0'
+AUTHOR = u'fnord0'
+TAGLINE = u'telecom/security/programming'
 SITEURL = 'http://localhost:8000'
+#SITEURL = 'http://fnord0.github.com'
 FEED_DOMAIN = SITEURL
 FEED_ATOM = 'feeds/all.atom.xml'
 FEED_RSS = 'feeds/all.rss'
-TIMEZONE = 'America/New_York'
+TIMEZONE = 'America/Los_Angeles'
 DEFAULT_LANG = u'en'
 DATE_FORMATS = {
     'en': '%Y-%m-%d',
 }
 DEFAULT_PAGINATION = 1
 
-THEME = 'themes/pelican-foundation'
+THEME = 'themes/html5-dopetrope'
 
 
 # display items
@@ -29,7 +30,7 @@ MENUITEMS = (
 )
 DISPLAY_PAGES_ON_MENU = True
 FOOTER_MESSAGE = u'This work is licensed under the <a href="http://creativecommons.org/licenses/by-sa/3.0/" rel="license">CC BY-SA</a>.'
-TWITTER_USERNAME = u'kfr_2'
+TWITTER_USERNAME = u'0xfnord'
 
 
 #STATIC_PATHS = ()
@@ -46,7 +47,7 @@ FILES_TO_COPY = (
 PLUGIN_PATH = 'pelican-plugins'
 PLUGINS = ('sitemap', 'gist', 'gist_comments')
 
-GITHUB_USERNAME = 'kfr2'
+GITHUB_USERNAME = 'fnord0'
 GITHUB_AUTH_TOKEN = environ.get('GITHUB_AUTH_TOKEN')
 if GITHUB_AUTH_TOKEN is None:
     raise NotImplementedError("You should define GITHUB_AUTH_TOKEN in your OS's environment.")
